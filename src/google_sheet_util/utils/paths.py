2f0ea304338022522d0a2d949561145328a09e69
agusherrera99 <agustinherrera.dev@gmail.com>
2026-01-07 13:07:18 -0300

    Move secrets dir and update ProjectPaths variables and remove unnecessary methods

--- src/google_sheet_util/utils/paths.py
+++ src/google_sheet_util/utils/paths.py
@@ -2,17 +2,12 @@ from pathlib import Path
 
 class ProjectPaths:
     def __init__(self):
-        self.ROOT_PATH = Path(__file__).resolve().parent.parent.parent.parent
+        self.ROOT_PATH = Path(__file__).resolve().parent.parent
         self.SECRETS_PATH = self.ROOT_PATH / "secrets"
-        self.SRC_PATH = self.ROOT_PATH / "src"
-
-    def get_root(self):
-        return self.ROOT_PATH
-
-    def get_src(self):
-        return self.SRC_PATH
 
     def get_secrets(self):
         return self.SECRETS_PATH
 
 project_paths = ProjectPaths()
+print(project_paths.ROOT_PATH)
+print(project_paths.SECRETS_PATH)
