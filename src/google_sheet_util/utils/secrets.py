5915acd69f45ac6ed1eb64023b30b472976a7ab1
agusherrera99 <agustinherrera.dev@gmail.com>
2026-01-07 13:41:29 -0300

    Remove get_content method, use json library methods and add more exceptions

--- src/google_sheet_util/utils/secrets.py
+++ src/google_sheet_util/utils/secrets.py
@@ -12,21 +12,21 @@ class Secret:
         self.SECRETS_PATH = base_path / "secrets"
         self.SECRETS_PATH.mkdir(parents=True, exist_ok=True)
 
-    def get_content(self, filepath: Path):
-        try:
-            with filepath.open("r", encoding="utf-8") as source_file:
-                content = source_file.read()
-            return content
-        except Exception as error:
-            print(f"Error al intentar obtener el contenido del archivo: {error}")
-
     def add_secret(self, filepath: Path):
         try:
+            with filepath.open("r", encoding="utf-8") as source_file:
+                data = json.load(source_file)
+
             destination = self.SECRETS_PATH / "credentials.json"
+
             with destination.open("w", encoding="utf-8") as newfile:
-                newfile.write(self.get_content(filepath))
+                json.dump(data, newfile, indent=2, ensure_ascii=False)
 
             print(f"Archivo secreto agegado exitosamente: {destination}")
+        except json.JSONDecodeError as error:
+            print(f"Error: El archivo no es un JSON válido - {error}")
+        except FileNotFoundError:
+            print(f"Error: No se encontró el archivo {filepath}")
         except Exception as error:
             print(f"Error al intentar agregar un archivo secreto: {error}")
 
