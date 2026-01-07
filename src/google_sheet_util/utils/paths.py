acea818ceafa103ce71e2f077494f416d9c0bdaf
agusherrera99 <agustinherrera.dev@gmail.com>
2026-01-07 13:01:22 -0300

    Update ProjectPaths variables

--- src/google_sheet_util/utils/paths.py
+++ src/google_sheet_util/utils/paths.py
@@ -2,9 +2,9 @@ from pathlib import Path
 
 class ProjectPaths:
     def __init__(self):
-        self.ROOT_PATH = Path(__file__).resolve().parent.parent
-        self.SRC_PATH = self.ROOT_PATH / "src"
+        self.ROOT_PATH = Path(__file__).resolve().parent.parent.parent.parent
         self.SECRETS_PATH = self.ROOT_PATH / "secrets"
+        self.SRC_PATH = self.ROOT_PATH / "src"
 
     def get_root(self):
         return self.ROOT_PATH
