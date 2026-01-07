8ce9fb7f26bcfa68c4530779da09b36fccc28d00
agusherrera99 <agustinherrera.dev@gmail.com>
2026-01-07 12:52:49 -0300

    Add get_content method and update add_secret method

--- src/google_sheet_util/utils/secrets.py
+++ src/google_sheet_util/utils/secrets.py
@@ -8,10 +8,21 @@ class Secret:
     def __init__(self):
         self.SECRETS_PATH = project_paths.get_secrets()
 
-    def add_secret(self, filepath: PosixPath):
+    def get_content(self, filepath: Path):
         try:
-            with filepath.open("w", encoding="utf-8") as newfile:
-                newfile.write(result)
+            with filepath.open("r", encoding="utf-8") as source_file:
+                content = source_file.read()
+            return content
+        except Exception as error:
+            print(f"Error al intentar obtener el contenido del archivo: {error}")
+
+    def add_secret(self, filepath: Path):
+        try:
+            destination = self.SECRETS_PATH / "credentials.json"
+            with destination.open("w", encoding="utf-8") as newfile:
+                newfile.write(self.get_content(filepath))
+
+            print(f"Archivo secreto agegado exitosamente: {destination}")
         except Exception as error:
             print(f"Error al intentar agregar un archivo secreto: {error}")
 
